415c62b6b2a97ae499f2a2f53f5ab6132faea2fc
Alex <romian37@gmail.com>
2026-01-04 19:30:26 +0700

    Fix Main App to use Dependency Injection after removing all static methods

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,23 @@
-import controller.EmployeeController;
-import view.MainMenuView;
+import service.CompetencyService;
+import service.EducationService;
+import service.EmployeeService;
+import service.ExperienceService;
+import service.impl.CompetencyServiceImpl;
+import service.impl.EducationServiceImpl;
+import service.impl.EmployeeServiceImpl;
+import service.impl.ExperienceServiceImpl;
+import view.EmployeeView;
+import view.MainView;
 
 public class Main {
     public static void main(String[] args) {
-        EmployeeController.initData();
-        MainMenuView.mainMenu();
+        EmployeeService employeeService = new EmployeeServiceImpl();
+        ExperienceService experienceService = new ExperienceServiceImpl();
+        EducationService educationService = new EducationServiceImpl();
+        CompetencyService competencyService = new CompetencyServiceImpl();
+
+        EmployeeView employeeView = new EmployeeView(employeeService, experienceService, educationService, competencyService);
+
+        new MainView(employeeView).show();
     }
 }
